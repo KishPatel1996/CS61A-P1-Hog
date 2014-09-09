dd76399d704461ba6c2c5ed923e84ca4d27780c6
Kishan Patel <kishanpatel@Kishans-MacBook-Pro-2.local>
2014-09-09 12:27:50 -0700

    update pycache.
    should have unlocked question 3

--- tests/q01.py
+++ tests/q01.py
@@ -8,42 +8,33 @@ test = {
   'suites': [
     [
       {
-        'locked': True,
         'test': """
         >>> roll_dice(2, make_test_dice(4, 6, 1))
-        56d669dcedf50b6144dee52c200c6a09
-        # locked
+        10
         """,
         'type': 'doctest'
       },
       {
-        'locked': True,
         'test': """
         >>> roll_dice(3, make_test_dice(4, 6, 1))
-        6e5521930a0f3da01e4348a227cc686a
-        # locked
+        1
         """,
         'type': 'doctest'
       },
       {
-        'locked': True,
         'test': """
         >>> roll_dice(3, make_test_dice(1, 2, 3))
-        6e5521930a0f3da01e4348a227cc686a
-        # locked
+        1
         """,
         'type': 'doctest'
       },
       {
-        'locked': True,
         'test': """
         >>> counted_dice = make_test_dice(4, 1, 2, 6)
         >>> roll_dice(3, counted_dice)
-        6e5521930a0f3da01e4348a227cc686a
-        # locked
+        1
         >>> roll_dice(1, counted_dice)  # Make sure you call dice exactly num_rolls times!
-        859fd5e4a281547b44754134af76b8ac
-        # locked
+        6
         """,
         'type': 'doctest'
       }
