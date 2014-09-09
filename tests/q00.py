ed872ce9a54a72c21090a1441b0963d2a0a3a697
Kishan Patel <kishanpatel@Kishans-MacBook-Pro-2.local>
2014-09-09 12:19:59 -0700

    completed implementation of problem 2
    boo-yah again

--- tests/q00.py
+++ tests/q00.py
@@ -8,16 +8,21 @@ test = {
   'suites': [
     [
       {
+        'locked': True,
         'test': """
         >>> test_dice = make_test_dice(4, 1, 2)
         >>> test_dice()
-        4
+        a4e96e54b9b170437d66dd7bd7e6c37c
+        # locked
         >>> test_dice() # Second call
-        1
+        6e5521930a0f3da01e4348a227cc686a
+        # locked
         >>> test_dice() # Third call
-        2
+        4024b7d247a7967fa973b60d80480da8
+        # locked
         >>> test_dice() # Fourth call
-        4
+        a4e96e54b9b170437d66dd7bd7e6c37c
+        # locked
         """,
         'type': 'doctest'
       }
